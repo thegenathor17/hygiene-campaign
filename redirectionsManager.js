28653e7ed816ff9626cae0c5c43f2de069917b87
Nahum <toodoo1703@gmail.com>
2024-04-26 16:57:48 -0600

    1.1.1

--- redirectionsManager.js
+++ redirectionsManager.js
@@ -1,5 +1,40 @@
-var button = false
-if (button == false)
-{
-    
-}
+function shower()
+        {
+            window.location.href = "showerBenefits.html"
+        }
+        function theet()
+        {
+            window.location.href = "theet.html"
+        }
+        function sheets()
+        {
+            window.location.href = "WhyINeedToChangeMySheetsFrequently.html"
+        }
+        function deodorant()
+        {
+            window.location.href = "WhyINeedToUseDeodorant.html"
+        }
+        function hands()
+        {
+            window.location.href = "whyINeedToWashMyHands.html"
+        }
+        function dirtClothes()
+        {
+            window.location.href = "whyiWontReutilizeMyDirtClothes.html"
+        }
+        function bathroom()
+        {
+            window.location.href = "whyINeedToCleanTheBathroom.html"
+        }
+        function us()
+        {
+            window.location.href = "aboutUs.html"
+        }
+        function campaign()
+        {
+            window.location.href = "aboutOurCampaign.html"
+        }
+        function credits()
+        {
+            window.location.href = "credits.html"
+        }
